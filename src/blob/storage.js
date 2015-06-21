458c4b3eceeb6e6502574a9db0e5eee9ac08c683
justin <mrjustinmooser@gmail.com>
2015-06-21 00:17:35 -0400

    update error messages for save method.

--- src/blob/storage.js
+++ src/blob/storage.js
@@ -141,18 +141,26 @@ module.exports = function (bucketName, provider) {
 
   s.save = function(bucket, key, blob) {
     return s.ready().then(function () {
-      return provider.putObject({
-        Key: key,
-        Body: JSON.stringify(blob),
-        Bucket: bucket
-      }).then(function () {
+      return p.resolve().then(function() {
+        return JSON.stringify(blob);
+      })
+      .then(function(body) {
+        return provider.putObject({
+          Key: key,
+          Body: body,
+          Bucket: bucket
+        })
+      })
+      .then(function () {
         return 's3://'+bucket+'/'+key;
       })
       .catch(function(err) {
         // TODO: check if error is 4xx and if it is just let it bubble up
         // TODO: else if it is 5xx do an exponential backoff retry.
         console.log('Failed to save a file to S3.  Details below:');
+        console.log(blob);
         console.error(err);
+        throw err;
       });
     });
   };
---
+++ src/nosql/dynamodb.js
@@ -0,0 +1,13 @@
+/**
+ * @module myModule
+ * @summary: This module's purpose is to:
+ *
+ * @description:
+ *
+ * Author: Justin Mooser
+ * Created On: 2015-06-16.
+ * @license Apache-2.0
+ */
+
+"use strict";
+
